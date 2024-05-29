0355303aa7027664149bc07a08b19e993d94321f
JakobSalmic <jakob.salmic@iskra.eu>
2024-05-29 07:58:49 +0200

    Changed version to 1.0.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pyiskra",
-    version="1.0.0",
+    version="1.0.1",
     description="Python Iskra devices interface",
     long_description=long_description,
     long_description_content_type="text/markdown",
