ffb6b279cf17ca006da91553e1f6219bc57a75e3
JakobSalmic <jakob.salmic@iskra.eu>
2024-06-06 15:15:03 +0200

    version as prerelease 0.1.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pyiskra",
-    version="1.0.2",
+    version="0.1.0",
     description="Python Iskra devices interface",
     long_description=long_description,
     long_description_content_type="text/markdown",
