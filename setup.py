59595772df240acd62517293d558e7e73c5304d1
JakobSalmic <jakob.salmic@iskra.eu>
2024-06-18 13:19:50 +0200

    bumped version to 0.1.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pyiskra",
-    version="0.1.1",
+    version="0.1.2",
     description="Python Iskra devices interface",
     long_description=long_description,
     long_description_content_type="text/markdown",
